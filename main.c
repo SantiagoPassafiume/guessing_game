cc1ed41caa096b86f47943e6fa5344823b10bf8e
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-07 13:02:20 -0900

    implement guess limit

--- main.c
+++ main.c
@@ -5,12 +5,32 @@ int main()
 {
     int secretNumber = 5;
     int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    int outOfGuesses = 0;
 
-    while (guess != secretNumber)
+    while (guess != secretNumber && outOfGuesses == 0)
     {
-        printf("Enter a number: ");
-        scanf("%d", &guess);
+        if (guessCount < guessLimit)
+        {
+            printf("Enter a number: ");
+            scanf("%d", &guess);
+            guessCount++;
+        }
+        else
+        {
+            outOfGuesses = 1;
+        }
     }
-    printf("You win!\n");
+
+    if (outOfGuesses)
+    {
+        printf("GAME OVER\n");
+    }
+    else
+    {
+        printf("You win!\n");
+    }
+
     return 0;
 }
