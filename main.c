3c0f4704c2f31b1cc6d1f26460a9b9759ac5eef0
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-02-07 12:53:30 -0900

    implement basic guessing game

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
+    int secretNumber = 5;
+    int guess;
 
+    while (guess != secretNumber)
+    {
+        printf("Enter a number: ");
+        scanf("%d", &guess);
+    }
+    printf("You win!\n");
     return 0;
 }
